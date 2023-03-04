b6b9831fe2a8ae79f590d0a551cdc7bfc35570f6
Home <murgu.adrian95@gmail.com>
2023-03-05 00:18:49 +0200

    rev2

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -9,7 +9,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:squad/main.dart';
 
-import "package:tema_1/main.dart";
+
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
